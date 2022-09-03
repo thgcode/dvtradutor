f35ac9be4c3c10d2bac7153b32f41a84e3685225
Thiago Seus <thiago.seus@yahoo.com.br>
2022-09-03 14:48:30 -0300

    Adicionar funcionalidade de escrever na tela

--- src/dvapi/testa.py
+++ src/dvapi/testa.py
@@ -1,9 +1,19 @@
 import requests
 ender = "http://127.0.0.1:8080"
+requests.post(f"{ender}/tela", json={"mensagem": "Programa de teste", "falando": False})
 
 requests.post(f"{ender}/fala", json={"mensagem": "Ola mundo!"})
 for i in range(10):
     requests.post(f"{ender}/bip")
     requests.post(f"{ender}/clek")
-m = requests.get(f"{ender}/linha", json={"falando": True})
-print(m.content)
+while True:
+    m = requests.get(f"{ender}/linha", json={"falando": True})
+    r = m.json()["resultado"]
+
+    requests.post(f"{ender}/tela", json={"mensagem": f"Voce escreveu {r}", "falando": True, "nova_linha": False})
+    requests.post(f"{ender}/tela", json={"mensagem": ".", "nova_linha": False, "falando": False})
+
+    if r == "sair":
+        break
+
+requests.post(f"{ender}/tela", json={"mensagem": "Fim do programa", "nova_linha": True, "falando": True})
--- src/dvapi/wrapDvWin.pas
+++ src/dvapi/wrapDvWin.pas
@@ -51,11 +51,57 @@ procedure leLinhaEndpoint(req: TRequest; resp: TResponse);
     end;
 end;
 
+procedure falandoEndpoint(req: TRequest; resp: TResponse);
+    var respObj: tJsonObject;
+    begin
+        try
+            respObj := tJsonObject.create;
+            respObj.booleans['resultado'] := sintFalando;
+            resp.content := respObj.asJson;
+        finally
+            respObj.free;
+        end;
+end;
+
+procedure escreveEndpoint(req: TRequest; resp: TResponse);
+    var o: tJsonObject;
+     s: string;
+    novaLinha: boolean;
+    falando: boolean;
+    begin
+    try
+        o := getJson(req.content) as tJsonObject;
+        s := o.strings['mensagem'];
+
+        novaLinha := o.get('nova_linha', true);
+        falando := o.get('falando', true);
+
+        if novaLinha then
+            begin
+            if falando then
+                sintWriteln(s)
+            else
+                writeln(s)
+        end
+        else
+            begin
+            if falando then
+                sintWrite(s)
+            else
+                write(s);
+        end;
+    finally
+        o.free;
+    end;
+end;
+
 procedure registra;
     begin
     HTTPRouter.RegisterRoute('/bip', rmPost, @bipaEndpoint);
     HTTPRouter.RegisterRoute('/clek', rmPost, @clekaEndpoint);
+    HTTPRouter.RegisterRoute('/falando', rmGet, @falandoEndpoint);
     HTTPRouter.RegisterRoute('/fala', rmPost, @sintetizaEndpoint);
     HTTPRouter.RegisterRoute('/linha', rmGet, @leLinhaEndpoint);
+    HTTPRouter.RegisterRoute('/tela', rmPost, @escreveEndpoint);
 end;
 end.
